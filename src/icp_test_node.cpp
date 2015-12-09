b5589458dd04f5a2ce1e40416f46b70e73d6d8cd
avavav <avavav@gmail.com>
2015-12-09 17:28:12 +0800

    with uniform sampling added

--- src/icp_test_node.cpp
+++ src/icp_test_node.cpp
@@ -10,6 +10,8 @@
 #include "message_filters/subscriber.h"
 #include <sensor_msgs/PointCloud2.h>
 
+#include <pcl/keypoints/uniform_sampling.h>
+
 #include <velodyne_pointcloud/rawdata.h>
 #include <velodyne_pointcloud/point_types.h>
 
@@ -54,21 +56,39 @@ class ICPProcess
 
     void pcCallback (const sensor_msgs::PointCloud2ConstPtr &scanMsg)
     {
-      //VPointCloud::Ptr outMsg(new VPointCloud());
+      //VPointCloud::Ptr orig_cloud(new VPointCloud());
+      pcl::PointCloud<pcl::PointXYZ>::Ptr orig_cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
-      //pcl::fromROSMsg(*scanMsg , *outMsg);
+      pcl::fromROSMsg(*scanMsg , *orig_cloud);
       pcl::fromROSMsg(*scanMsg , *cloud_out);
       //outMsg->header.stamp = pcl_conversions::toPCL(scanMsg->header).stamp;
       //outMsg->header.frame_id = "base_imu";
       cloud_out->header.stamp = pcl_conversions::toPCL(scanMsg->header).stamp;
       cloud_out->header.frame_id = "base_imu";
+      orig_cloud->header.stamp = pcl_conversions::toPCL(scanMsg->header).stamp;
+      orig_cloud->header.frame_id = "base_imu";
+
+
+
+      pcl::PointCloud<int> indices_out;
+      pcl::UniformSampling<pcl::PointXYZ> uniform_sampling;
+      uniform_sampling.setInputCloud (orig_cloud);
+      uniform_sampling.setRadiusSearch (0.05f);
+      //uniform_sampling.filter (*model_keypoints);
+      uniform_sampling.compute (indices_out);
+      std::cout << "Model total points: " << orig_cloud->size () << "; Selected Keypoints: " << indices_out.size () << std::endl;
+      pcl::copyPointCloud(*orig_cloud, indices_out.points, *cloud_out);
+
+      //pcl::PointCloud<int> indices_out;
+      //uniform_sampling.setInputCloud (cloud_in);
+      //uniform_sampling.compute (*indices_out);
+      //pcl::copyPointCloud(*orig_cloud, indices_out.points, *cloud_out);
 
       if (!first_time)
       {
+	pcl::PointCloud<pcl::PointXYZ>::Ptr Final (new pcl::PointCloud<pcl::PointXYZ>);
 	icp.setInputCloud(cloud_in);
 	icp.setInputTarget(cloud_out);
-	//pcl::PointCloud<pcl::PointXYZ> Final;
-	pcl::PointCloud<pcl::PointXYZ>::Ptr Final (new pcl::PointCloud<pcl::PointXYZ>);
 	icp.align(*Final);
 
 	std::cout << "has converged:" << icp.hasConverged() << " score: " <<
