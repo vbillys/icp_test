7b1bd66cc6f87dd5198b9e88a8e70bc6b9fa7da3
avavav <avavav@gmail.com>
2015-12-13 18:45:17 +0800

    added gicp implementation plus unisampling rosparam parameterization

--- src/icp_test_node.cpp
+++ src/icp_test_node.cpp
@@ -3,6 +3,10 @@
 #include <pcl/point_types.h>
 #include <pcl/registration/icp.h>
 
+#include <pcl/surface/organized_fast_mesh.h>
+#include <pcl/registration/gicp.h>
+#include <pcl/features/from_meshes.h>
+
 #include <ros/ros.h>
 #include <pcl_ros/point_cloud.h>
 
@@ -35,14 +39,26 @@ class ICPProcess
     ros::Publisher  pub_;
     pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
     //pcl::IterativeClosestPoint<VPoint, VPoint> icp;
+    pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> gicp;
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in  ;//(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out ;//(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZ>::Ptr orig_cloud;//(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr orig_cloud_in;//(new pcl::PointCloud<pcl::PointXYZ>);
     //pcl::PointCloud<pcl::PointXYZ> cloud_in  ;//(new pcl::PointCloud<pcl::PointXYZ>);
     //pcl::PointCloud<pcl::PointXYZ> cloud_out ;//(new pcl::PointCloud<pcl::PointXYZ>);
 
+    pcl::PolygonMesh::Ptr mesh;
+    pcl::OrganizedFastMesh<pcl::PointXYZ> fast_mesh;
+    pcl::PointCloud<pcl::Normal>::Ptr normals;
+    //boost::shared_ptr< std::vector<Eigen::Vector3d> > covs;
+    //std::vector<Eigen::Matrix3d, Eigen::aligned_allocator<Eigen::Matrix3d> >  covs_out;
+    //std::vector<Eigen::Matrix3d, Eigen::aligned_allocator<Eigen::Matrix3d> >  covs_in;
+    pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ>::MatricesVectorPtr covs_out;
+    pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ>::MatricesVectorPtr covs_in;
+
     bool first_time;
+    double uni_sampling_factor_;
 
     Eigen::Matrix4d transformation_matrix;
     Eigen::Matrix3d rot_matrix;
@@ -52,14 +68,23 @@ class ICPProcess
     tf::Transform curr_transform;
 
   public:
-    ICPProcess(ros::NodeHandle & nh)
+    ICPProcess(ros::NodeHandle & nh, double & uni_sampling_factor)
     {
       pub_ = nh.advertise<sensor_msgs::PointCloud2>( "filtered_points", 1);
       first_time = true;
+      uni_sampling_factor_ = uni_sampling_factor;
 
       cloud_in  = pcl::PointCloud<pcl::PointXYZ>::Ptr (new pcl::PointCloud<pcl::PointXYZ>);
       cloud_out = pcl::PointCloud<pcl::PointXYZ>::Ptr (new pcl::PointCloud<pcl::PointXYZ>);
       orig_cloud= pcl::PointCloud<pcl::PointXYZ>::Ptr (new pcl::PointCloud<pcl::PointXYZ>);
+      orig_cloud_in= pcl::PointCloud<pcl::PointXYZ>::Ptr (new pcl::PointCloud<pcl::PointXYZ>);
+
+      normals = pcl::PointCloud<pcl::Normal>::Ptr (new pcl::PointCloud<pcl::Normal>);
+      mesh = pcl::PolygonMesh::Ptr (new pcl::PolygonMesh);
+
+      covs_in =     pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ>::MatricesVectorPtr (new     pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ>::MatricesVector) ;
+      covs_out =     pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ>::MatricesVectorPtr (new     pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ>::MatricesVector) ;
+
 
       //icp.setMaximumIterations(50);
       // Set the max correspondence distance to 5cm (e.g., correspondences with higher distances will be ignored)
@@ -72,6 +97,8 @@ class ICPProcess
       //icp.setTransformationEstimation (trans_lls);
 
       curr_transform.setIdentity();
+
+      gicp.setMaxCorrespondenceDistance(1);
     }
     ~ICPProcess()
     {
@@ -102,28 +129,50 @@ class ICPProcess
       pcl::PointCloud<int> indices_out;
       pcl::UniformSampling<pcl::PointXYZ> uniform_sampling;
       uniform_sampling.setInputCloud (orig_cloud);
-      uniform_sampling.setRadiusSearch (0.050f);
+      //uniform_sampling.setRadiusSearch (0.150f);
+      uniform_sampling.setRadiusSearch (uni_sampling_factor_);
       //uniform_sampling.filter (*model_keypoints);
-      uniform_sampling.compute (indices_out);
-      std::cout << "Model total points: " << orig_cloud->size () << "; Selected Keypoints: " << indices_out.size () << std::endl;
-      pcl::copyPointCloud(*orig_cloud, indices_out.points, *cloud_out);
+      //uniform_sampling.compute (indices_out);
+
+      //*cloud_out = * orig_cloud;
+      uniform_sampling.filter (*cloud_out);
+      std::cout << "Model total points: " << orig_cloud->size () << "; Selected Keypoints: " << cloud_out->size () << std::endl;
+
+      //std::cout << "Model total points: " << orig_cloud->size () << "; Selected Keypoints: " << indices_out.size () << std::endl;
+      //pcl::copyPointCloud(*orig_cloud, indices_out.points, *cloud_out);
 
       //pcl::PointCloud<int> indices_out;
       //uniform_sampling.setInputCloud (cloud_in);
       //uniform_sampling.compute (*indices_out);
       //pcl::copyPointCloud(*orig_cloud, indices_out.points, *cloud_out);
 
+      //fast_mesh.setInputCloud(cloud_out);
+      //fast_mesh.reconstruct( * mesh);
+
+      //pcl::features::computeApproximateNormals( *cloud_out, mesh->polygons, *normals);
+      //pcl::features::computeApproximateCovariances( * cloud_out, * normals, *covs_out);
+
       if (!first_time)
       {
-	pcl::PointCloud<pcl::PointXYZ>::Ptr Final (new pcl::PointCloud<pcl::PointXYZ>);
-	icp.setInputCloud(cloud_in);
-	icp.setInputTarget(cloud_out);
-	icp.align(*Final);
 
-	std::cout << "has converged:" << icp.hasConverged() << " score: " <<
-	  icp.getFitnessScore() << std::endl;
+	gicp.setInputSource(cloud_in);
+	gicp.setInputTarget(cloud_out);
+	//gicp.setSourceCovariances(covs_in);
+	//gicp.setTargetCovariances(covs_out);
+
+	pcl::PointCloud<pcl::PointXYZ>::Ptr Final (new pcl::PointCloud<pcl::PointXYZ>);
+	//icp.setInputCloud(cloud_in);
+	//icp.setInputTarget(cloud_out);
+	//icp.align(*Final);
+	gicp.align(*Final);
+
+	std::cout << "has converged:" << gicp.hasConverged() << " score: " <<
+	  gicp.getFitnessScore() << std::endl;
+	//std::cout << "has converged:" << icp.hasConverged() << " score: " <<
+	  //icp.getFitnessScore() << std::endl;
 	//std::cout << icp.getFinalTransformation() << std::endl;
-	transformation_matrix = icp.getFinalTransformation ().cast<double>();
+	//transformation_matrix = icp.getFinalTransformation ().cast<double>();
+	transformation_matrix = gicp.getFinalTransformation ().cast<double>();
 	//trans_vector << transformation_matrix(0,3),transformation_matrix(1,3),transformation_matrix(2,3);
 	tf::Matrix3x3 tf3d;
 	tf3d.setValue(transformation_matrix(0,0),transformation_matrix(0,1),transformation_matrix(0,2)
@@ -154,6 +203,8 @@ class ICPProcess
       //pub_.publish(cloud_out);
       *cloud_in = *cloud_out;
       //cloud_in = cloud_out;
+      *covs_in = * covs_out;
+      *orig_cloud_in = *orig_cloud;
 
       first_time = false;
 
@@ -162,13 +213,15 @@ class ICPProcess
       std::cout << "Elapsed:" << ddur << std::endl;
 
       // Obtain the transformation that aligned cloud_source to cloud_source_registered
-       Eigen::Matrix4f transformation = icp.getFinalTransformation ();
+       //Eigen::Matrix4f transformation = icp.getFinalTransformation ();
+       Eigen::Matrix4f transformation = gicp.getFinalTransformation ();
 
 
       pcl::registration::TransformationValidationEuclidean<pcl::PointXYZ, pcl::PointXYZ> tve;
       tve.setMaxRange (0.01);  // 1cm
       tve.setThreshold (0.01);
-      double score = tve.validateTransformation (cloud_in, cloud_out, transformation);
+      //double score = tve.validateTransformation (cloud_in, cloud_out, transformation);
+      double score = tve.validateTransformation (orig_cloud_in, orig_cloud, transformation);
       std::cout << "Verify:" << score << " " << tve.isValid(cloud_in, cloud_out, transformation) << std::endl;
 
     }
@@ -181,12 +234,16 @@ int main (int argc, char** argv)
 {
   ros::init(argc, argv, "icp_test_node");
   ros::NodeHandle nh;//("~");
+  //ros::NodeHandle nh("~");
   ros::Rate r(10);
 
-  ICPProcess icp_process(nh);
+  double uni_sampling_factor;
+  //nh.getParam("uniform_sampling", uni_sampling_factor);
+  nh.param("/icp_test_node/uniform_sampling", uni_sampling_factor, 0.15);
+
+  ICPProcess icp_process(nh, uni_sampling_factor);
 
   ros::Subscriber sub = nh.subscribe ("velodyne_points", 1 , &ICPProcess::pcCallback, &icp_process);
-
 
   while (ros::ok()){ros::spinOnce();}//ROS_INFO_STREAM("Hello, world!");r.sleep();}
 
